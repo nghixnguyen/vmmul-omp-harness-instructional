a6de7a5961f9f7319f972fdbbb636697f27b8dc0
nghi_nguyen <nghinshopping@gmail.com>
2024-11-21 15:47:22 -0800

    modify vector

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -6,7 +6,7 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  * where A is n-by-n matrix stored in row-major format, and X and Y are n by 1 vectors.
  * On exit, A and X maintain their input values.
  */
-void my_dgemv(int n, double* A, double* x, double* y) {
+void my_dgemv(int n, double A[], double x[], double y[]) {
    for (int i = 0; i < n; i++) {
         double temp = 0.0;
         for (int j = 0; j < n; j++) {
